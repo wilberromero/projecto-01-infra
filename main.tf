2c47fa765f8ad26dbf32adfa10d0a8349ca6780c
wilberromero <wilber.romero@davivienda.com>
2023-12-15 19:34:02 -0500

    correccion

--- main.tf
+++ main.tf
@@ -208,6 +208,8 @@ terraform {
         bucket = "example-infra23"
         key    = "terraform.tfstate"
         region = "us-east-1"
+        access_key = "AKIAVXLCKSOAGNQYMXZN"
+        secret_key = "wZ3Qn8nq6JRYo69X9MLPr98L+jqc38iegbpUzpqH"
     }
 }
 
