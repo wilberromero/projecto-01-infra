ba504390741ead35f0f77a334fb773e4a6a479ba
wilberromero <wilber.romero@davivienda.com>
2023-12-20 14:12:15 -0500

    yyttt

--- variables.tf
+++ variables.tf
@@ -36,7 +36,3 @@ variable "availability_zone_2" {
     default = "us-east-1b"
 }
 
-# these are zones and subnets examples
-availability_zones = ["us-east-1a", "us-east-1b"]
-public_subnets     = ["10.10.100.0/24", "10.10.101.0/24"]
-private_subnets    = ["10.10.0.0/24", "10.10.1.0/24"]
