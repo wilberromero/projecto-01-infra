7a093b9d2bca5d762e5ab3925e84a450e2a6fec4
wilberromero <wilber.romero@davivienda.com>
2023-12-25 13:45:53 -0500

    role Ecs

--- modules/iamrole/main.tf
+++ modules/iamrole/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     })
 }
 
-resource "aws_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
     role = aws_iam_role.ecs_task_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEcsTaskExecutionRolePolicy"
 }
