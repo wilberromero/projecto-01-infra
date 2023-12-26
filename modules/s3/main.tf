13c9a4182a4b8202431122733a3daaff88b58c3b
wilberromero <wilber.romero@davivienda.com>
2023-12-25 20:47:03 -0500

    cloudfront

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -3,3 +3,20 @@ resource "aws_s3_bucket" "example_infra23" {
   bucket = "example-infra23"  
 
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.example_infra23.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      Sid = "GrantCloudFrontAccess",
+      Effect = "Allow"
+      Principal = {
+        AWS = "*"
+      },
+      Action = "s3:GetObject",
+      Resource = "${aws_s3_bucket.example_infra23.arn}/*"
+    ]
+  })
+}
