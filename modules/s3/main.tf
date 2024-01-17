981edc765cb5e9cd095fed6b60e17aff2c94f525
wilberromero <wilber.romero@davivienda.com>
2024-01-17 18:05:15 -0500

    s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,23 +6,24 @@ resource "aws_s3_bucket" "example_infra23" {
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
-    actions   = ["s3:GetObject",
+    actions   = [
+      "s3:GetObject",
       "s3:GetObject",
       "s3:GetObjectAcl",
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:DeleteObject",
-      "s3:*",
-      "s3:ListBucket"
+      "s3:*"
     ]
     resources = ["${aws_s3_bucket.example_infra23.arn}/*"]
-       
+
     principals {
       type        = "AWS"
       identifiers = [var.aws_cloudfront_OAI_arn_from_networking_module]
     }
   }
 }
+
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.example_infra23.id
