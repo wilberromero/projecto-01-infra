06605f2d639f102cf7664396cbebb4271deae2bd
wilberromero <wilber.romero@davivienda.com>
2024-01-17 18:58:52 -0500

    s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -25,6 +25,8 @@ data "aws_iam_policy_document" "s3_policy" {
 }
 
 
+
+
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.example_infra23.id
   policy = data.aws_iam_policy_document.s3_policy.json
